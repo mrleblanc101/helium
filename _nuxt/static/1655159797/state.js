6ef461a15b51ab41162635021ebfd4a63f5e28bb
mrleblanc101 <mrleblanc101@users.noreply.github.com>
2022-06-13 23:40:54 +0000

    deploy: 4f1addced293449d4db40183efbd70c2ffb5b607

--- _nuxt/static/1655159797/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhelium\u002F_nuxt\u002Fstatic\u002F1655159797",layout:"default",error:a,serverRendered:true,routePath:"\u002F",config:{_app:{basePath:"\u002Fhelium\u002F",assetsPath:"\u002Fhelium\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"e73b55e5"}}}}(null));
