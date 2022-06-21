a732aad7fa9d4d9654291f67cffccb7b41975ea2
mrleblanc101 <mrleblanc101@users.noreply.github.com>
2022-06-21 17:02:20 +0000

    deploy: 2024eda732280cd211dd9108fd611ba5cbb9edb0

---
+++ _nuxt/static/1655830939/agence/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhelium\u002F_nuxt\u002Fstatic\u002F1655830939",layout:"default",error:a,serverRendered:true,routePath:"\u002Fagence",config:{_app:{basePath:"\u002Fhelium\u002F",assetsPath:"\u002Fhelium\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"615747e6"}}}}(null));
