703b1ed98d47b06c628cb04ae45bf57ba5971f7b
Sébastien LeBlanc <sebastien.leblanc@libeo.com>
2022-06-27 14:24:00 -0400

    Add soial links

--- components/NavSocial.vue
+++ components/NavSocial.vue
@@ -2,33 +2,30 @@
     <nav class="nav-social">
         <ul>
             <li>
-                <!-- TODO: Lien -->
-                <a href="" target="_blank">
+                <a href="https://www.facebook.com/agencehelium" target="_blank">
                     <span class="visuallyhidden">Facebook</span>
                     <img inline class="icon-social" src="@/assets/svg/facebook.svg" />
                 </a>
             </li>
             <li>
-                <!-- TODO: Lien -->
-                <a href="" target="_blank">
+                <a href="https://www.instagram.com/agencehelium_" target="_blank">
                     <span class="visuallyhidden">Instagram</span>
                     <img inline class="icon-social" src="@/assets/svg/instagram.svg" />
                 </a>
             </li>
             <li>
-                <!-- TODO: Lien -->
-                <a href="" target="_blank">
+                <a href="https://www.behance.net/agencehelium" target="_blank">
                     <span class="visuallyhidden">Behance</span>
                     <img inline class="icon-social" src="@/assets/svg/behance.svg" />
                 </a>
             </li>
-            <li>
-                <!-- TODO: Lien -->
+            <!-- TODO: Lien -->
+            <!-- <li>
                 <a href="" target="_blank">
                     <span class="visuallyhidden">LinkedIn</span>
                     <img inline class="icon-social" src="@/assets/svg/linkedin.svg" />
                 </a>
-            </li>
+            </li> -->
         </ul>
     </nav>
 </template>
