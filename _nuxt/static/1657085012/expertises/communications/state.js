4f60b9bc17ae304bdabcd8807db25eeae3186ad1
mrleblanc101 <mrleblanc101@users.noreply.github.com>
2022-07-06 05:32:56 +0000

    deploy: dee56c90931298028f20efcad3999786119ead2e

--- _nuxt/static/1657085012/expertises/communications/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002Fhelium\u002F_nuxt\u002Fstatic\u002F1657085012",layout:"default",error:a,serverRendered:true,routePath:"\u002Fexpertises\u002Fcommunications",config:{_app:{basePath:"\u002Fhelium\u002F",assetsPath:"\u002Fhelium\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"bac4aaae"}}}}(null));
