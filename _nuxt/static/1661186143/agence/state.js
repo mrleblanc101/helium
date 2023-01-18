291b902043f0cf4371c83fa31dacf965c2dec1c6
mrleblanc101 <mrleblanc101@users.noreply.github.com>
2023-01-18 22:26:19 +0000

    deploy: 2a333000f4261ac89774c7d496ea4ddd1f138359

--- _nuxt/static/1661186143/agence/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1661186143",layout:"default",error:a,serverRendered:true,routePath:"\u002Fagence",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"961313a9"}},_img:{"/_ipx/q_60/img/photo-vincent.png":"\u002F_nuxt\u002Fimage\u002F3a8dc9.png"}}}(null));
---
+++ _nuxt/static/1674080710/expertises/deploiement/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1674080710",layout:"default",error:a,serverRendered:true,routePath:"\u002Fexpertises\u002Fdeploiement",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"076b7900"}}}}(null));
